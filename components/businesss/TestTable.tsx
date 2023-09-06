48d57c52d3300b61b1a74a60849ae4540789c7a5
DanishAnsari5522 <ddanish1928@gmail.com>
2023-09-06 10:40:21 +0530

    fix bug 4

--- components/businesss/TestTable.tsx
+++ components/businesss/TestTable.tsx
@@ -102,7 +102,7 @@ export const TestTable = () => {
                                 </div>
                             </td>
                             <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                Apple MacBook Pro 17"
+                                Apple MacBook Pro 17
                             </th>
                             <td className="px-6 py-4">
                                 Silver
